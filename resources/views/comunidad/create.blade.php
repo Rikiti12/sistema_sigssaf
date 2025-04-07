5c9ad27ae58d5449c34b464bfa757cb595db5bb3
Rikiti12 <rikisandooval12@gmail.com>
2025-04-07 12:01:47 -0400

    verificar la comunidad.

--- resources/views/comunidad/create.blade.php
+++ resources/views/comunidad/create.blade.php
@@ -12,14 +12,14 @@
                 <div class="card mb-4">
 
                     <div class="card-header py-3 d-flex flex-row align-items-center justify-content-center">
-    
+
                     <h2 class="font-weight-bold text-dark">Registrar comunidad</h2>
-    
+
                     </div>
 
                     <form method="post" action="{{ route('comunidad.store') }}" enctype="multipart/form-data" onsubmit="return Comunidad(this)">
                         @csrf
-                            
+
                         <div class="card-body">
 
                             <center>
@@ -27,27 +27,22 @@
                             </center>
 
                             <br>
-                            
+
                             <div class="row">
 
                                 <div class="col-4">
                                     <label  class="font-weight-bold text-dark">Cedula Vocero</label>
                                     <input type="text" class="form-control" id="cedula_jefe" name="cedula_jefe" maxlength="8" style="background: white;" value="" placeholder="Ingrese La Cedula" autocomplete="off" onkeypress="return solonum(event);">
                                 </div>
-        
-                                <div class="col-4">
-                                    <label  class="font-weight-bold text-dark">Nombre Vocero</label>
-                                    <input type="text" class="form-control" id="nom_jefe" name="nom_jefe" style="background: white;" value="" placeholder="Ingrese El Nombre" oninput="capitalizarInput('nombre')" autocomplete="off" onkeypress="return soloLetras(event);">
-                                </div>
-        
-                                <div class="col-4">
-                                    <label  class="font-weight-bold text-dark">Apellido Vocero</label>
-                                    <input type="text" class="form-control" id="ape_jefe" name="ape_jefe" style="background: white;" value="" placeholder="Ingrese El Apellido" autocomplete="off"  oninput="capitalizarInput('apellido')" onkeypress="return soloLetras(event);">
-                                </div>
 
                                 <div class="col-4">
                                     <label  class="font-weight-bold text-dark">Nombre Vocero</label>
                                     <input type="text" class="form-control" id="nom_jefe" name="nom_jefe" style="background: white;" value="" placeholder="Ingrese El Nombre" oninput="capitalizarInput('nombre')" autocomplete="off" onkeypress="return soloLetras(event);">
+                                </div>
+
+                                <div class="col-4">
+                                    <label  class="font-weight-bold text-dark">Apellido Vocero</label>
+                                    <input type="text" class="form-control" id="ape_jefe" name="ape_jefe" style="background: white;" value="" placeholder="Ingrese El Apellido" autocomplete="off"  oninput="capitalizarInput('apellido')" onkeypress="return soloLetras(event);">
                                 </div>
 
                                 <div class="col-4">
@@ -56,7 +51,7 @@
                                 </div>
 
                             </div>
-                        
+
                         </div>
 
                         <div class="card-body">
@@ -76,9 +71,9 @@
 
                                 <div class="col-4">
                                     <label  class="font-weight-bold text-dark">Dirección de la Comunidad</label>
-                                    <textarea class="form-control" id="direccion" name="dire_comuni" cols="10" rows="10" style="max-height: 6rem;" oninput="capitalizarInput('direccion')">{{ old('direccion') }}</textarea>                                   
-                                </div> 
-                                
+                                    <textarea class="form-control" id="direccion" name="dire_comuni" cols="10" rows="10" style="max-height: 6rem;" oninput="capitalizarInput('direccion')">{{ old('direccion') }}</textarea>
+                                </div>
+
                                 <div class="col-4">
                                     <label  class="font-weight-bold text-dark">Comuna Asignado</label>
                                     <select class="form-select" id="c_comuna" name="id_comuna">
@@ -86,31 +81,43 @@
                                         @foreach($comunas as $comuna)
                                             <option value="{{ $comuna->id }}">{{ $comuna->nom_comunas }}</option>
                                         @endforeach
-                                    </select>                                   
+                                    </select>
                                 </div>
-                                
+
                             </div>
 
-                            
                             <br>
 
                             <div class="row">
-
-                                <center>
-                                    <h5 class="font-weight-bold text-dark">Crear Proyecto</h5>
-                                </center>
-
-                                <div class="col-4">
-                                    <label  class="font-weight-bold text-dark">Nombre de Proyecto</label>
-                                    <input type="text" class="form-control" id="nom_proyecto" name="nom_proyecto" style="background: white;" value="" placeholder="Ingrese El nombre del proyecto" autocomplete="off" oninput="capitalizarInput('nombre comuna')" onkeypress="return soloLetras(event);">
+                                <div class="col-12">
+                                    <label class="font-weight-bold text-dark">¿Desea crear un proyecto para esta comunidad?</label>
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="crear_proyecto" id="si_proyecto" value="si" onchange="mostrarOcultarProyecto()">
+                                        <label class="form-check-label" for="si_proyecto">Sí</label>
+                                    </div>
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="crear_proyecto" id="no_proyecto" value="no" onchange="mostrarOcultarProyecto()" checked>
+                                        <label class="form-check-label" for="no_proyecto">No</label>
+                                    </div>
                                 </div>
+                            </div>
 
-                                <div class="col-4">
-                                    <label  class="font-weight-bold text-dark">Descripción del Proyecto</label>
-                                    <textarea class="form-control" id="descripcion" name="descripcion" cols="10" rows="10" style="max-height: 6rem;" oninput="capitalizarInput('descripcion')">{{ old('descripcion') }}</textarea>                                   
-                                </div> 
-                                
-                                
+                            <div id="datos_proyecto" style="display: none;">
+                                <br>
+                                <center>
+                                    <h5 class="font-weight-bold text-dark">Datos del Proyecto</h5>
+                                </center>
+                                <div class="row">
+                                    <div class="col-6">
+                                        <label  class="font-weight-bold text-dark">Nombre del Proyecto</label>
+                                        <input type="text" class="form-control" id="nom_proyecto" name="nom_proyecto" style="background: white;" value="" placeholder="Ingrese El nombre del proyecto" autocomplete="off" oninput="capitalizarInput('nombre proyecto')" onkeypress="return soloLetras(event);">
+                                    </div>
+
+                                    <div class="col-6">
+                                        <label  class="font-weight-bold text-dark">Descripción del Proyecto</label>
+                                        <textarea class="form-control" id="descripcion" name="descripcion" cols="10" rows="5" style="max-height: 6rem;" oninput="capitalizarInput('descripcion proyecto')">{{ old('descripcion') }}</textarea>
+                                    </div>
+                                </div>
                             </div>
 
                             <br><br>
@@ -120,9 +127,9 @@
                                 <span class="text">Guardar</span>
                                 </button>
                                 <a  class="btn btn-info btn-lg" href="{{ url('comunidad/') }}"><span class="icon text-white-50">
-                                    <i class="fas fa-info-circle"></i>
-                                </span>
-                                <span class="text">Regresar</span></a>
+                                        <i class="fas fa-info-circle"></i>
+                                    </span>
+                                    <span class="text">Regresar</span></a>
                             </center>
 
                         </div>
@@ -143,6 +150,20 @@
         function capitalizarInput(idInput) {
             const inputElement = document.getElementById(idInput);
             inputElement.value = capitalizarPrimeraLetra(inputElement.value);
+        }
+
+        function mostrarOcultarProyecto() {
+            const siProyecto = document.getElementById('si_proyecto');
+            const datosProyectoDiv = document.getElementById('datos_proyecto');
+
+            if (siProyecto.checked) {
+                datosProyectoDiv.style.display = 'block';
+            } else {
+                datosProyectoDiv.style.display = 'none';
+                // Limpiar los campos del proyecto si se ocultan
+                document.getElementById('nom_proyecto').value = '';
+                document.getElementById('descripcion').value = '';
+            }
         }
     </script>
 
--- resources/views/comunidad/edit.blade.php
+++ resources/views/comunidad/edit.blade.php
@@ -12,15 +12,15 @@
                 <div class="card mb-4">
 
                     <div class="card-header py-3 d-flex flex-row align-items-center justify-content-center">
-    
+
                     <h2 class="font-weight-bold text-dark">Actualizar Comunidad</h2>
-    
+
                     </div>
 
                     <form method="post" action="{{ url('/comunidad/'.$comunidad->id) }}" enctype="multipart/form-data" onsubmit="return Comunidad(this)">
                         @csrf
                         {{ method_field('PATCH')}}
-                            
+
                         <div class="card-body">
 
                             <center>
@@ -28,30 +28,30 @@
                             </center>
 
                             <br>
-                            
+
                             <div class="row">
-        
+
                                 <div class="col-4">
                                     <label  class="font-weight-bold text-dark">Cédula Vocero</label>
-                                    <input type="text" class="form-control" id="cedula" name="cedula_jefe" maxlength="8" style="background: white;" value="{{ isset($comunidad->cedula_jefe)?$comunidad->cedula_jefe:'' }}" placeholder="Ingrese La Cédula" autocomplete="off" onkeypress="return solonum(event);">
+                                    <input type="text" class="form-control" id="cedula" name="cedula_jefe" maxlength="8" style="background: white;" value="{{ $comunidad->cedula_jefe ?? '' }}" placeholder="Ingrese La Cédula" autocomplete="off" onkeypress="return solonum(event);">
                                 </div>
-        
+
                                 <div class="col-4">
                                     <label  class="font-weight-bold text-dark">Nombre Vocero</label>
-                                    <input type="text" class="form-control" id="nombre" name="nom_jefe" style="background: white;" value="{{ isset($comunidad->nom_jefe)?$comunidad->nom_jefe:'' }}" placeholder="Ingrese El Nombre" oninput="capitalizarInput('nombre')" autocomplete="off" onkeypress="return soloLetras(event);">
+                                    <input type="text" class="form-control" id="nombre" name="nom_jefe" style="background: white;" value="{{ $comunidad->nom_jefe ?? '' }}" placeholder="Ingrese El Nombre" oninput="capitalizarInput('nombre')" autocomplete="off" onkeypress="return soloLetras(event);">
                                 </div>
-        
+
                                 <div class="col-4">
                                     <label  class="font-weight-bold text-dark">Apellido Vocero</label>
-                                    <input type="text" class="form-control" id="apellido" name="ape_jefe" style="background: white;" value="{{ isset($comunidad->ape_jefe)?$comunidad->ape_jefe:'' }}" placeholder="Ingrese El Apellido" autocomplete="off"  oninput="capitalizarInput('apellido')" onkeypress="return soloLetras(event);">
+                                    <input type="text" class="form-control" id="apellido" name="ape_jefe" style="background: white;" value="{{ $comunidad->ape_jefe ?? '' }}" placeholder="Ingrese El Apellido" autocomplete="off"  oninput="capitalizarInput('apellido')" onkeypress="return soloLetras(event);">
                                 </div>
                                 <div class="col-4">
                                     <label  class="font-weight-bold text-dark">Telefono</label>
-                                    <input type="text" class="form-control" id="telefono" name="telefono" maxlength="12" style="background: white;" value="{{ isset($comunidad->telefono)?$comunidad->telefono:'' }}" placeholder="Ingrese El Telefono" autocomplete="off" onkeypress="return solonum(event);">
+                                    <input type="text" class="form-control" id="telefono" name="telefono" maxlength="12" style="background: white;" value="{{ $comunidad->telefono ?? '' }}" placeholder="Ingrese El Telefono" autocomplete="off" onkeypress="return solonum(event);">
                                 </div>
-                              
+
                             </div>
-                        
+
                         </div>
 
                         <div class="card-body">
@@ -61,26 +61,26 @@
                             </center>
 
                             <br>
-                            
+
                             <div class="row">
 
                                 <div class="col-4">
                                     <label  class="font-weight-bold text-dark">Nombre de la Comunidad</label>
-                                    <input type="text" class="form-control" id="comunidad" name="nom_comuni" style="background: white;" value="{{ isset($comunidad->nom_comuni)?$comunidad->nom_comuni:'' }}" placeholder="Ingrese El nombre comunidad" autocomplete="off" oninput="capitalizarInput('nombre comuna')" onkeypress="return soloLetras(event);">
+                                    <input type="text" class="form-control" id="comunidad" name="nom_comuni" style="background: white;" value="{{ $comunidad->nom_comuni ?? '' }}" placeholder="Ingrese El nombre comunidad" autocomplete="off" oninput="capitalizarInput('nombre comuna')" onkeypress="return soloLetras(event);">
                                 </div>
 
                                 <div class="col-4">
                                     <label  class="font-weight-bold text-dark">Dirección</label>
-                                    <textarea class="form-control" id="direccion" name="dire_comuni" cols="10" rows="10" style="max-height: 6rem;" oninput="capitalizarInput('direccion')">{{ $comunidad->dire_comuni }}</textarea>                                   
+                                    <textarea class="form-control" id="direccion" name="dire_comuni" cols="10" rows="10" style="max-height: 6rem;" oninput="capitalizarInput('direccion')">{{ $comunidad->dire_comuni ?? '' }}</textarea>
                                 </div>
 
                                 <div class="col-4">
                                     <label  class="font-weight-bold text-dark">Comuna Asignado</label>
                                     <select class="form-select" id="id_comuna" name="id_comuna">
                                         @foreach($comunas as $comuna)
-                                            <option value="{{ $comuna->id }}" @selected($comuna->id_comuna == $comuna->id)>{{ $comuna->nom_comunas }}</option>
+                                            <option value="{{ $comuna->id }}" @selected($comunidad->id_comuna == $comuna->id)>{{ $comuna->nom_comunas }}</option>
                                         @endforeach
-                                    </select>                                   
+                                    </select>
                                 </div>
 
                             </div>
@@ -88,22 +88,35 @@
                             <br>
 
                             <div class="row">
-
-                                <center>
-                                    <h5 class="font-weight-bold text-dark">Editar Proyecto</h5>
-                                </center>
-
-                                <div class="col-4">
-                                    <label  class="font-weight-bold text-dark">Nombre de Proyecto</label>
-                                    <input type="text" class="form-control" id="nom_proyecto" name="nom_proyecto" style="background: white;" value="{{ isset($comunidad->nom_proyecto)?$comunidad->nom_proyecto:'' }}" autocomplete="off" oninput="capitalizarInput('nom_proyecto')" onkeypress="return soloLetras(event);">
+                                <div class="col-12">
+                                    <label class="font-weight-bold text-dark">¿Desea editar el proyecto de esta comunidad?</label>
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="editar_proyecto" id="si_proyecto" value="si" onchange="mostrarOcultarProyecto()" {{ !empty($comunidad->nom_proyecto) ? 'checked' : '' }}>
+                                        <label class="form-check-label" for="si_proyecto">Sí</label>
+                                    </div>
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="editar_proyecto" id="no_proyecto" value="no" onchange="mostrarOcultarProyecto()" {{ empty($comunidad->nom_proyecto) ? 'checked' : '' }}>
+                                        <label class="form-check-label" for="no_proyecto">No</label>
+                                    </div>
                                 </div>
+                            </div>
 
-                                <div class="col-4">
-                                    <label  class="font-weight-bold text-dark">Descripción del Proyecto</label>
-                                    <textarea class="form-control" id="descripcion" name="descripcion" cols="10" rows="10" style="max-height: 6rem;" oninput="capitalizarInput('descripcion')">{{ $comunidad->descripcion }}</textarea>                                   
-                                </div> 
-                                
-                                
+                            <div id="datos_proyecto" style="display: {{ !empty($comunidad->nom_proyecto) ? 'block' : 'none' }};">
+                                <br>
+                                <center>
+                                    <h5 class="font-weight-bold text-dark">Datos del Proyecto</h5>
+                                </center>
+                                <div class="row">
+                                    <div class="col-6">
+                                        <label  class="font-weight-bold text-dark">Nombre de Proyecto</label>
+                                        <input type="text" class="form-control" id="nom_proyecto" name="nom_proyecto" style="background: white;" value="{{ $comunidad->nom_proyecto ?? '' }}" autocomplete="off" oninput="capitalizarInput('nom_proyecto')" onkeypress="return soloLetras(event);">
+                                    </div>
+
+                                    <div class="col-6">
+                                        <label  class="font-weight-bold text-dark">Descripción del Proyecto</label>
+                                        <textarea class="form-control" id="descripcion" name="descripcion" cols="10" rows="5" style="max-height: 6rem;" oninput="capitalizarInput('descripcion')">{{ $comunidad->descripcion ?? '' }}</textarea>
+                                    </div>
+                                </div>
                             </div>
 
 
@@ -114,9 +127,9 @@
                                 <span class="text">Guardar</span>
                                 </button>
                                 <a  class="btn btn-info btn-lg" href="{{ url('comunidad/') }}"><span class="icon text-white-50">
-                                    <i class="fas fa-info-circle"></i>
-                                </span>
-                                <span class="text">Regresar</span></a>
+                                        <i class="fas fa-info-circle"></i>
+                                    </span>
+                                    <span class="text">Regresar</span></a>
                             </center>
 
                         </div>
@@ -125,7 +138,7 @@
 
                 </div>
 
-            </div> 
+            </div>
 
     </div>
 
@@ -138,6 +151,25 @@
             const inputElement = document.getElementById(idInput);
             inputElement.value = capitalizarPrimeraLetra(inputElement.value);
         }
+
+        function mostrarOcultarProyecto() {
+            const siProyecto = document.getElementById('si_proyecto');
+            const datosProyectoDiv = document.getElementById('datos_proyecto');
+
+            if (siProyecto.checked) {
+                datosProyectoDiv.style.display = 'block';
+            } else {
+                datosProyectoDiv.style.display = 'none';
+                // Limpiar los campos del proyecto si se ocultan (opcional en edición)
+                // document.getElementById('nom_proyecto').value = '';
+                // document.getElementById('descripcion').value = '';
+            }
+        }
+
+        // Ejecutar la función al cargar la página para establecer la visibilidad inicial
+        document.addEventListener('DOMContentLoaded', function() {
+            mostrarOcultarProyecto();
+        });
     </script>
 
     @if ($errors->any())
